cde6825992dc8a7afccc640a3736a52a895fc109
shanerivera <103073265+shanerivera@users.noreply.github.com>
2022-04-05 15:37:33 -0500

    Add files via upload

---
+++ Project_01/JLetter.java
@@ -0,0 +1,26 @@
+/** 
+* Prints letter J as a large block letter 
+* composed of the characters ‘J’, ‘A’, ‘V’, ‘A’.
+* @author Shane Rivera - COMP1210 - 005
+* @version 1/19/2022
+*/
+public class JLetter
+{
+/**
+     * Prints a big letter J using ‘J’, ‘A’, ‘V’, ‘A’.
+     * 
+     * @param args Command line arguments (not used).
+     */
+   public static void main(String[] args) {
+      System.out.println("JAVAJAVAJAVA");
+      System.out.println("JAVAJAVAJAVA");
+      System.out.println("      JAVA   ");
+      System.out.println("      JAVA   ");
+      System.out.println("      JAVA   ");
+      System.out.println("      JAVA   ");
+      System.out.println("J     JAVA   ");
+      System.out.println("JA    JAVA   ");
+      System.out.println(" JAVAJAVA ");
+      System.out.println("  JAVAJA  ");
+   }
+}
---
+++ Project_01/MyLifeGoals.java
@@ -0,0 +1,31 @@
+/** 
+ * Prints my life goals (short, medium, and long term).
+ *
+ * @author Shane Rivera - COMP1210 - 005
+ * @version 1/19/2022
+ */
+public class MyLifeGoals
+{
+/**
+    * Prints name and then short term goals, medium term goals
+    * and long term goals on separate lines.
+    * @param args Command line arguments (not used).
+    */
+   public static void main(String[] args) {
+      System.out.println("Shane Rivera");
+      System.out.println();
+      System.out.println("My short-term goals are to build a foundation "
+            + "in programming, which means doing well in these next "
+            + "few COMP classes. Also, I want to figure out what "
+            + "exactly I want to do with computer science in the future.");
+      System.out.println("My medium-term goals are to do internships in "
+            + "areas of computer science that interest me to build "
+            + "my resume and start taking classes that are more specific "
+            + "to furthering my academic career that will help me get a job.");
+      System.out.println("Finally, my long-term goals are to land a job "
+            + "that I enjoy that also (hopefully) makes a lot of money. "
+            + "I will work for as long as I need to while investing and "
+            + "buying real estate until I have enough passive income to "
+            + "retire and have financially freedom.");
+   }
+}
